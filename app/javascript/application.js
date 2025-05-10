3b70fd2ee651846797cb9137195c4fb36e4688e2
Hiromii88 <goeson88@gmail.com>
2025-05-10 19:56:19 +0900

    top_page menu_bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".menu-button");
+  const menuBar = document.querySelector(".menu-bar");
+
+  if (menuButton && menuBar) {
+    menuButton.addEventListener("click", () => {
+      menuBar.classList.toggle("open");
+    });
+
+    document.addEventListener("click", (e) => {
+      if (!menuBar.contains(e.target) && !menuButton.contains(e.target)) {
+        menuBar.classList.remove("open");
+      }
+    });
+  }
+});
